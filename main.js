575435c163e2641dc607f1d6e7ebfd1be44d5a49
aditinayak312 <83701303+aditinayak312@users.noreply.github.com>
2022-05-16 15:44:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump= loadSound("jump.wav");
+	mario_coins= loadSound("coin.wav");
+	mario_game_over= loadSound("gameover.wav");
+	mario_kick= loadSound("kick.wav");
+	mario_die= loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent("canvas");
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent("game_console");
+	poseNet=ml5.poseNet(video,modelLoaded);
+	poseNet.on("pose", gotResults);
+}
+
+function modelLoaded(){
+	console.log("modelLoaded");
+}
+
+function gotResults(results){
+	if (results.length>0){
+		console.log(results);
+		noseX=results[0].pose.nose.x;
+		noseY=results[0].pose.nose.y;
+	}
+}
+
+function draw(){
+	game();
+}
+
+
+
+
+
+
